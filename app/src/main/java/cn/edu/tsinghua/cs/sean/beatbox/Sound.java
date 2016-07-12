4533ffb789b718b51d08250eeb07058e08d605ce
Sean-Lan <sean.lan.thu@gmail.com>
2016-07-12 11:15:07 +0800

    Add assets handling logic

---
+++ app/src/main/java/cn/edu/tsinghua/cs/sean/beatbox/Sound.java
@@ -0,0 +1,25 @@
+package cn.edu.tsinghua.cs.sean.beatbox;
+
+/**
+ * Created by lx on 16/7/12.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+}
